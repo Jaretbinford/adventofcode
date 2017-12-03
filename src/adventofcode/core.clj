c86997993427f17f66c649a9f375e94fa75935dd
Jaret Binford <jaretbinford@gmail.com>
2017-12-03 10:09:39 -0500

    day 2

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -66,6 +66,37 @@
 (countstuffagain input)
 
 
+;; Day 2!
+
+(ns adventofcode.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(def input (->>
+            (io/resource "data2")
+            io/reader
+            line-seq
+            (mapv (comp (partial mapv read-string)
+                        #(string/split % #"\s+")))))
+
+(defn checksum [row]
+  (->> ((juxt (partial reduce min) (partial reduce max)) row)
+       (apply -)
+       (Math/abs)))
+
+(apply + (map checksum input))
+
+
+(defn checksumagain [row]
+  (first (for [a row
+               b row
+               :when (and (not= a b)
+                          (= 0 (rem a b)))]
+           (/ a b))))
+
+
+(apply + (map checksumagain input))
+
 
 
 
