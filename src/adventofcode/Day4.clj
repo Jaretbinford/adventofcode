2358c97ef35f225d0d1bbffdc0f8895639a0a77b
Jaret Binford <jaretbinford@gmail.com>
2017-12-05 00:14:04 -0500

    Day 4

---
+++ src/adventofcode/Day4.clj
@@ -0,0 +1,39 @@
+(ns adventofcode.Day4
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io])
+  (:refer-clojure :exclude [boolean? ex-cause ex-message uuid uuid? random-uuid]))
+
+(def input (->>
+             (io/resource "data3")
+             io/reader
+             line-seq
+             (mapv (comp (partial mapv read-string)
+                         #(string/split % #"\s+")))))
+(comment
+    input
+    ;for each input
+    (input 1)
+    (frequencies (input 1))
+    (apply distinct? (input 1)))
+
+(count (filter #(= (count (distinct %)) (count %))
+               input))
+
+;; easier to do with a string instead of a vector:
+(def input-string (slurp (io/resource "data3")))
+
+(->>
+  input-string
+  string/split-lines
+  (map #(string/split % #" "))
+  (map (fn [line] (map sort line)))
+  (filter #(= (count %) (count (set %))))
+  count)
+
+
+
+
+
+
+
+
